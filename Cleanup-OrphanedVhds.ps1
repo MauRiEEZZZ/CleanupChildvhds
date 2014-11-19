098ca2786e111975f4539a353b5d8fcc73ec4edc
Maurice MJ de Jong <maurieezzz@hotmail.com>
2014-11-19 14:30:30 +0100

    FirstCommit

---
+++ Cleanup-OrphanedVhds.ps1
@@ -0,0 +1,10 @@
+﻿$computername = "."
+
+$vhdsOnDisk = gci -LiteralPath ((Get-VMHost -ComputerName $computername).VirtualHardDiskPath) -Include "*.vhd?" -Attributes !Readonly
+$vhdsUsed = @()
+foreach ($vm in (Get-VM -ComputerName $computername)) {
+    Write-Verbose $vm.VMName
+    foreach ($vhd in $vm.HardDrives) {
+        $vhdsUsed += $vhd.Path
+    }
+}
